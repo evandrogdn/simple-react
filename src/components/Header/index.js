a932f8f260f5ab9c3756d027da4c63371938abd0
evandro_gdn <evandrogardolinn@gmail.com>
2019-11-09 09:15:40 -0300

    [ADD] #components - Adicionado componente para o header da aplicação

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+import { Container, Content } from './styles';
+
+const Header = () => (
+    <Container>
+        <Content>
+            <nav>
+                <Link></Link>
+            </nav>
+        </Content>
+    </Container>
+);
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Container = styeld.div`
+    background: #004870;
+    padding: 0 30px;
+`;
+
+export const Content = styled.div`
+    height: 64px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    nav{
+        display: flex;
+        align-items: center;
+        img{
+            margin-right: 20px;
+            padding-right: 20px;
+            border-right: 1px solid #ed00e1;
+            width: 70px;
+            height: 50px;
+        }
+        a{
+            font-weight: bold;
+            color: #eee;
+        }
+    }
+    aside{
+        display: flex;
+        align-items: center;
+    }
+`;
