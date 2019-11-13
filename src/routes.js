4f3b486439dc0361d5b0d9d8e519179f01d19198
evandro_gdn <evandrogardolinn@gmail.com>
2019-11-12 23:24:59 -0300

    [ADD] #route - Adicionada rota para produtos

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Estoque from "./pages/Estoque";
 import Fornecedor from "./pages/Fornecedor";
+import Produto from "./pages/Produto";
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/estoque" exact component={Estoque} /> 
+                <Route path="/estoque" exact component={Estoque} />
                 <Route path="/fornecedor" exact component={Fornecedor} />
+                <Route path="/produto" exact component={Produto} />
             </Switch>
         </BrowserRouter>
     );
