50dc48657dd8c3d1cddcee24e9f28d6280317453
evandro_gdn <evandrogardolinn@gmail.com>
2019-11-12 22:14:09 -0300

    [ADD] #routes - Adicionada rota para operaçoes com cadastro de fornecedores

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Estoque from "./pages/Estoque";
+import Fornecedor from "./pages/Fornecedor";
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/estoque" exact component={Estoque} /> 
+                <Route path="/fornecedor" exact component={Fornecedor} />
             </Switch>
         </BrowserRouter>
     );
