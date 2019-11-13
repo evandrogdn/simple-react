52a51a945ec9d2c1f94b0f9ab98d9d632b01ab86
evandro_gdn <evandrogardolinn@gmail.com>
2019-11-13 07:35:26 -0300

    [ADD] #routes - Adicionadas rotas de Provisa e Unidade de Medida

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Estoque from "./pages/Estoque";
 import Fornecedor from "./pages/Fornecedor";
 import Produto from "./pages/Produto";
+import Provisao from "./pages/Provisao";
+import UnidadeMedida from "./pages/UnidadeMedida";
 
 export default function Routes() {
     return (
@@ -12,6 +14,8 @@ export default function Routes() {
                 <Route path="/estoque" exact component={Estoque} />
                 <Route path="/fornecedor" exact component={Fornecedor} />
                 <Route path="/produto" exact component={Produto} />
+                <Route path="/provisao" exact component={Provisao} />
+                <Route path="/unidade-medida" exact component={UnidadeMedida} />
             </Switch>
         </BrowserRouter>
     );
