5371df81cc2b08ea74b1cce18979552e2c394669
evandro_gdn <evandrogardolinn@gmail.com>
2019-11-12 23:26:00 -0300

    [ADD] #route - Adicionado link para tela de produtos

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 import { Container, Content } from './styles';
 
 const Header = () => (
-    <Container>
-        <Content>
-            <nav>
-                <Link to="/estoque">Estoque</Link>
-                <Link to="/fornecedor">Fornecedor</Link>
-            </nav>
-        </Content>
-    </Container>
+  <Container>
+    <Content>
+      <nav>
+        <Link to="/estoque">Estoque</Link>
+        <Link to="/fornecedor">Fornecedor</Link>
+        <Link to="/produto">Produto</Link>
+      </nav>
+    </Content>
+  </Container>
 );
 
 export default Header;
